eca8a58909065fcf02a24f9ac5e67d44ab50a59a
Richard Couget <finanzassmartia2025@gmail.com>
2026-05-27 17:48:19 -0300

    SEO base configurado

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+export default function sitemap() {
+
+  return [
+    {
+      url: "https://richardcouget.com",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://richardcouget.com/en",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://richardcouget.com/blog/problema-financiero-no-es-matematico",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://richardcouget.com/blog/primero-la-vida-despues-el-sistema-financiero",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://richardcouget.com/blog/agotamiento-mental-y-desorden-financiero",
+      lastModified: new Date(),
+    },
+  ];
+}
